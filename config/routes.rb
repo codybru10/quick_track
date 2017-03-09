ec6baf851b2c5dfe1b33e5b63bec137f931504ce
Cody Brubaker <cody.brubaker@gmail.com>
2017-03-09 11:44:16 -0800

    add rails admin gem for adminstration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :employees, :controllers => {registrations: 'registrations'}
 
   root to: "clients#index"
